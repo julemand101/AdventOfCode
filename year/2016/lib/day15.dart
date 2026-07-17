da2266dbe348f346e2f9d11b7c27bfedc2d9284c
Jacob Bang <julemand101@archlinux.dk>
2026-07-17 11:12:49 +0200

    Simplify 2016/15 a bit

--- year/2016/lib/day15.dart
+++ year/2016/lib/day15.dart
@@ -2,23 +2,24 @@
 // https://adventofcode.com/2016/day/15
 
 final inputPattern = RegExp(
-  r'Disc #(\d+) has (\d+) positions; at time=(\d+), it is at position (\d+).',
+  r'Disc #(\d+) has (\d+) positions; at time=0, it is at position (\d+).',
 );
 
 int solve(Iterable<String> input) {
-  List<bool Function(int time)> discs = [];
+  List<({int id, int positions, int positionAtTime})> discs = [];
 
   for (final match in input.map(inputPattern.firstMatch).nonNulls) {
     final id = int.parse(match[1]!);
     final positions = int.parse(match[2]!);
-    //final time = int.parse(match[3]!);
-    final positionAtTime = int.parse(match[4]!);
+    final positionAtTime = int.parse(match[3]!);
 
-    discs.add((time) => ((positionAtTime + time + id) % positions) == 0);
+    discs.add((id: id, positions: positions, positionAtTime: positionAtTime));
   }
 
   for (var time = 0; true; time++) {
-    if (discs.every((f) => f(time))) {
+    if (discs.every(
+      (disc) => ((disc.positionAtTime + time + disc.id) % disc.positions) == 0,
+    )) {
       return time;
     }
   }
